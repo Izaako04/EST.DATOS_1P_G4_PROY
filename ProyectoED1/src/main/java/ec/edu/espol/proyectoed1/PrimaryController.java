ee42087b48ba35c12c90c65412c726e74bac81e9
isaiasgh <joelisaiasguamaniherrera@gmail.com>
2024-05-17 18:49:33 -0500

    commit1

--- ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/PrimaryController.java
+++ ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/PrimaryController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    // comentario
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
