a091c65ffa72b45fd0cea97b9e4bba36889920fc
Izaako04 <isaac.criollo04@gmail.com>
2024-05-31 16:53:03 -0500

    Papelera Usuario en Sistema

--- ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/Sistema.java
+++ ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/Sistema.java
@@ -4,6 +4,15 @@
  */
 package ec.edu.espol.proyectoed1;
 
+import ec.edu.espol.proyectoed1.TDAs.ArrayListG4;
+import ec.edu.espol.proyectoed1.classes.Usuario;
+import ec.edu.espol.proyectoed1.classes.Utilitaria;
+import static ec.edu.espol.proyectoed1.classes.Utilitaria.escribirArchivo;
+import static ec.edu.espol.proyectoed1.classes.Utilitaria.leerArchivo;
+import ec.edu.espol.proyectoed1.classes.Vehiculo;
+import ec.edu.espol.proyectoed1.classes.excepcionDatoNoExistente;
+import java.util.List;
+
 /**
  *
  * @author Isaías
@@ -11,4 +20,32 @@ package ec.edu.espol.proyectoed1;
 
 public class Sistema {
     
+    public void agregarUsuario_Archivo(Usuario u){
+    
+        if (leerArchivo("usuarios")== null){
+          List<Usuario> usuarios= new ArrayListG4<>();
+          usuarios.add(u);
+          escribirArchivo(usuarios,"usuarios" );
+      }
+    else{
+          List<Usuario> usuarios= leerArchivo("usuarios");
+          usuarios.add(u);
+          escribirArchivo(usuarios, "usuarios");
+      }
+        
+    }
+    
+    public static boolean verificarContraseia_ARCHIVO(String correo, String contrasenia){
+        List<Usuario> usuarios = leerArchivo("usuario");
+        if (usuarios != null) {
+            for (Usuario user : usuarios) {
+                if (!user.getCorreo().equals(correo) && !user.getContrasenia().equals(contrasenia)) {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+        return false;
+    }
 }
