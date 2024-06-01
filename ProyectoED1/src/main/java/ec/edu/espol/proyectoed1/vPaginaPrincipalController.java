19bb28f8ba7eb167da3a235ea637a738686156f2
isaiasgh <joguherr@espol.edu.ec>
2024-06-01 17:23:54 -0500

    Configuracion del visualizador de imagenes de la Pagina Principal, ya se puede guardar nuevos vehiculos

--- ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/vPaginaPrincipalController.java
+++ ProyectoED1/src/main/java/ec/edu/espol/proyectoed1/vPaginaPrincipalController.java
@@ -5,6 +5,7 @@
 package ec.edu.espol.proyectoed1;
 
 import ec.edu.espol.proyectoed1.TDAs.ArrayListG4;
+import ec.edu.espol.proyectoed1.TDAs.CDLinkedList;
 import ec.edu.espol.proyectoed1.classes.Usuario;
 import java.io.IOException;
 import java.net.URL;
@@ -22,7 +23,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import ec.edu.espol.proyectoed1.classes.Filtrable;
+import ec.edu.espol.proyectoed1.classes.Utilitaria;
 import ec.edu.espol.proyectoed1.classes.Vehiculo;
+import java.io.File;
 import java.net.URL;
 import java.util.Comparator;
 import java.util.List;
@@ -31,10 +34,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -82,9 +95,23 @@ public class vPaginaPrincipalController implements Initializable, Filtrable {
     private Scene scene;
 
     
+    private int widthSpane = 604;
+    private int heightSpane = 522;
+    
+    private int widthImg = 182;
+    private int heightImg = 128;
+   
+    private int widthBtn = 128;
+    private int heightBtn = 34;
+    
+    private Usuario usuario;
+    
     private List<Vehiculo> vehiculos;
+    @FXML
+    private ScrollPane scrollPane;
 
-
+    private String estilo2 = "-fx-background-color: transparent; -fx-text-fill: transparent;";
+    
     /**
      * Initializes the controller class.
      */
@@ -103,11 +130,8 @@ public class vPaginaPrincipalController implements Initializable, Filtrable {
     
 
     public void home (Usuario user) {
-        for (Vehiculo v: user.getVehiculosPropios()) {
-            System.out.println(v.getRegistro().getPlaca());
-        }
-        
-        textoSaludoUsuario.setText("Bienvenido " + user.getNombre());
+        usuario = user;
+        textoSaludoUsuario.setText("Bienvenido " + usuario.getNombre());
         
         btnVenderVehiculo.setOnAction(event -> {
             try {
@@ -123,6 +147,83 @@ public class vPaginaPrincipalController implements Initializable, Filtrable {
         cmbMarca.setItems(keyList);
         
         cmbMarca.setOnAction(event -> configuraComboBox (marcaYModelo));
+        
+        
+        
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        
+        cargarVehiculos ();
+    }
+    
+    private void cargarVehiculos() {
+        CDLinkedList <Vehiculo> CDLLVehiculos = Utilitaria.leerArchivoVehiculos("vehiculos");
+        
+        int cantidadVehiculos = CDLLVehiculos.size();
+        int j = 0;
+        
+        if(cantidadVehiculos != 0){
+            int cantFilas = (int) Math.ceil(cantidadVehiculos/3.0);
+            int tope = 3;
+
+            for(int i = 0; i < cantFilas; i++){
+                HBox hbox = new HBox();
+                
+                while(j < cantidadVehiculos & j < tope){
+                    Vehiculo vehiculo = CDLLVehiculos.get(j);
+                    
+                    StackPane spane = new StackPane();
+                    spane.setAlignment(Pos.CENTER);
+                    spane.setPrefSize(widthSpane, heightSpane);
+
+                    ImageView visualizadorImg = new ImageView();
+                    visualizadorImg.setFitHeight(heightImg);
+                    visualizadorImg.setFitWidth(widthImg);
+
+                    File imageFile = vehiculo.getCdLLImagenes().get(0);
+                    Image image = new Image(imageFile.toURI().toString());
+                    visualizadorImg.setImage(image);
+
+                    Button btnSeleccionar = new Button("Seleccionar");
+                    btnSeleccionar.setPrefSize(widthBtn, heightBtn);
+                    btnSeleccionar.setStyle(estilo2);
+                    btnSeleccionar.setCursor(Cursor.HAND);
+                    
+                    spane.getChildren().addAll(visualizadorImg, btnSeleccionar);
+
+                    hbox.getChildren().addAll(spane);
+                    
+                    // Manejar el evento cuando el cursor entra del botón
+                    visualizadorImg.setOnMouseEntered(event -> estiloHover(btnSeleccionar, visualizadorImg));
+                    btnSeleccionar.setOnMouseEntered(event -> estiloHover(btnSeleccionar, visualizadorImg));
+
+                    // Manejar el evento cuando el cursor sale del botón
+                    visualizadorImg.setOnMouseExited(event -> estiloNotHover(btnSeleccionar, visualizadorImg));
+                    btnSeleccionar.setOnMouseExited(event -> estiloNotHover(btnSeleccionar, visualizadorImg));
+                    
+                    // btnSeleccionar.setOnAction(event -> ); implementar cambiar a vista modo circular!
+                    
+                    j++;
+                }
+                
+                hbox.setSpacing(0.5);
+                tope += 3;
+                contenedorHbox.getChildren().addAll(hbox);   
+            
+            }
+            
+        }
+        contenedorHbox.setSpacing(20);
+    }
+    
+    public void estiloHover(Button btnAnadirCod, ImageView img){
+        btnAnadirCod.setStyle("-fx-background-color: #980B20; -fx-background-radius: 10; -fx-text-fill: white; cursor: hand;");
+        img.setOpacity(0.5);
+        btnAnadirCod.setFont(new Font("Verdana", 14));
+    }
+    
+    public void estiloNotHover(Button btnAnadirCod, ImageView img){
+        btnAnadirCod.setStyle("-fx-background-color: transparent; -fx-text-fill: transparent;");
+        img.setOpacity(1.0);
     }
     
     private void configuraComboBox (Map <String, ArrayListG4 <String> > marcaYModelo){
